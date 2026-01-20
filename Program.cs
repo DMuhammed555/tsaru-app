251ad10a86ca01284628866ee13af83f8db7d5a4
Muiza <Cat@Lovelas@gmail.com>
2026-01-20 10:29:32 +0300

    Добавил форму

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Tsaru {
+    class Program  {
+        static void Main()
+        {
+            System.Console.WriteLine("Hi there");
+            System.Console.WriteLine("We are wathing you!");
+            Form.GetForm();
+            Console.ReadLine();
+        }
+      
+    }
+}
